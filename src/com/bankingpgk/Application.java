baf069261c7f7794cc93c8bf743995af35331d95
Vovanviet <vietvvth2107012@fpt.edu.vn>
2022-03-26 13:23:19 +0700

    Create Application.java

---
+++ src/com/bankingpgk/Application.java
@@ -0,0 +1,8 @@
+package com.bankingpgk;
+
+public class Application {
+    public static void main(String[] args) {
+        Controller controller=new Controller();
+        controller.home();
+    }
+}
