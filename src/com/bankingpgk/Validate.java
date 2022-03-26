7b4094909b580903f4c0cd6d2b967f275912c92c
Vovanviet <vietvvth2107012@fpt.edu.vn>
2022-03-26 13:23:28 +0700

    Create Validate.java

---
+++ src/com/bankingpgk/Validate.java
@@ -0,0 +1,28 @@
+package com.bankingpgk;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validate {
+    public static final Pattern SerialRegex=Pattern.compile(
+    "(?=.*[0-9])+[0-9]{13}");
+    public static final Pattern PasswordRegex=Pattern.compile(
+            "(?=.*[a-z])(?=.*[A-Z])(?=.*[.,-_;]).{7,15}",Pattern.CASE_INSENSITIVE);
+    public static String validateSerial(String serial){
+        Matcher matcher=SerialRegex.matcher(serial);
+        if (matcher.find()){
+            return serial;
+
+        }else {
+            throw new RuntimeException("Serial Not Invalid");
+        }
+    }
+    public static String validatePassword(String password){
+        Matcher matcher=PasswordRegex.matcher(password);
+        if (matcher.find()){
+            return password;
+        }else {
+            throw new RuntimeException("Password Not Invalid");
+        }
+    }
+}
