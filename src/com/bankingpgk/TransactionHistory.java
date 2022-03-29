bc405cfd36986fdcddcc1dfe61077f00b40a178e
Vovanviet <vietvvth2107012@fpt.edu.vn>
2022-03-29 17:19:42 +0700

    Create TransactionHistory.java

---
+++ src/com/bankingpgk/TransactionHistory.java
@@ -0,0 +1,71 @@
+package com.bankingpgk;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class TransactionHistory {
+    private long id;
+    private LocalDate dayTrading;
+    private String description;
+    private String beneficiaryAccount;
+    private Double money;
+
+    public TransactionHistory( String description, String beneficiaryAccount, Double money) {
+        this.id = IdGenerate.getNewId();
+        this.dayTrading = LocalDate.now();
+        this.description = description;
+        this.beneficiaryAccount = beneficiaryAccount;
+        this.money = money;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDayTrading() {
+        return dayTrading;
+    }
+
+    public void setDayTrading(LocalDate dayTrading) {
+        this.dayTrading = dayTrading;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getBeneficiaryAccount() {
+        return beneficiaryAccount;
+    }
+
+    public void setBeneficiaryAccount(String beneficiaryAccount) {
+        this.beneficiaryAccount = beneficiaryAccount;
+    }
+
+    public Double getMoney() {
+        return money;
+    }
+
+    public void setMoney(Double money) {
+        this.money = money;
+    }
+
+    @Override
+    public  String toString() {
+        DateTimeFormatter myFormatter=DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return "TransactionHistory: " +"\n"+
+                " id: " + id +"\n"+
+                " dayTrading: " + dayTrading.format(myFormatter) +"\n"+
+                " description: " + description + '\n' +
+                " beneficiaryAccount: " + beneficiaryAccount + '\n' +
+                " money: " + money+"VND" ;
+    }
+}
